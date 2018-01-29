03703ed7ce3d8e3f37c0d5132ed0f443ae451d5a
Hannah Hamavid <hamavid@Hannahs-computer.local>
2018-01-28 17:53:54 -0800

    adding shows photos as slideshows to media page, updating year in footer

---
+++ src/js/Media.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+// DIV SIZING
+	function set_show_heights() {
+	    var photowidth=$('.thisphoto').width();
+	    var smaller=0.7*photowidth;
+	    if (photowidth>760 || smaller > 370) {
+	      $('.thisphoto').css('height','370px');
+	    } else {
+	    	console.log('smaller');
+	      $('.thisphoto').css('height',smaller+'px');
+	    }  
+	  }
+	  $(window).bind('resize',set_show_heights); 
+	  set_show_heights();
+
+
+// SLIDESHOWS
+  // Create a dictionary of img names for each show 
+  // key is show name, which should = id of div these images will be shown in, 
+  // as well as the name of the directory that has the relevant files
+	var showdict = {};
+	showdict['bubbes'] = ['20171216_213355', 'DSC01442', 'DSC01431',
+		'DSC01434', 'DSC01470', 'DSC01477', 'DSC01588','20171216_213353',
+		'DSC01466', '25440158_10213236172671598_2710990013775537959_o', 'DSC01417'];
+	showdict['misc'] = ['houseshow 2018-1-20', '17-08-04-pocket-theater', '17-11-18-paloma'];
+
+  // on page load the 0th image will be shown for each show, so trigger the fxn to say this in the captions
+	function start() {for (var key in showdict) {enumerate(key,0,showdict[key].length);}}
+	start();
+
+ // function to look up which pictures to show in a given slideshow, using the id of the div and the show dictionary
+  	function whichpics(show) {for (var key in showdict) {if (show.includes(key)) {return showdict[key];}};};
+
+  // function to show a given image in the slides for a given show
+  	function showslideimg(show, picname){
+  		$('#'+show+' figure img').remove(); //remove extra photos
+  		$('#'+show+' figure').prepend($('<img/>').attr('src','../images/shows/'+show+'/'+picname+'.jpg'));
+  	}
+  
+  // function to say which number out of total photos this photo is for a given show
+  	function enumerate(show, num, total){
+  		var thisnum=num+1;
+  		$('#'+show+' .counter').html(' ('+thisnum+'/'+total+')');
+  	}
+
+  // function to iterate back and forward through images in slideshow
+  	function iterate(show, direction) {
+  		var srcList = whichpics(show);
+  		var thisimg=$('#'+show+' figure img').attr('src').replace('../images/shows/'+show+'/','');
+		for (var i = 0; i < srcList.length; i++) {if (srcList[i]+'.jpg' == thisimg){var thisindex=i;}};
+		var nextindex=(thisindex+direction) % srcList.length;
+		if (nextindex < 0) {var nextindex = srcList.length-1}
+		var nextimg=srcList[nextindex];
+		showslideimg(show, nextimg);
+		enumerate(show, nextindex, srcList.length);
+  	}
+
+  // highlight left and right scroll arrows when hovering on diff areas of slideshow div
+	$('.leftside').mouseenter(function(){$('.larr').css('opacity','1');});
+	$('.leftside').mouseleave(function(){$('.larr').css('opacity','0.3');});
+	$('.rightside').mouseenter(function(){$('.rarr').css('opacity','1');});
+	$('.rightside').mouseleave(function(){$('.rarr').css('opacity','0.3');});
+
+  // Scroll right or left when various elements are clicked
+	$('.larr, .leftside').click(function() {iterate($(this).parent().parent().attr('id'), -1);
+	});
+	$('.rarr, .rightside').click(function() {iterate($(this).parent().parent().attr('id'), +1);});
+
+  // SWIPE  
+    $('.thisphoto').touchwipe({
+      wipeLeft: function() {iterate($(this).parent().attr('id'), +1);},
+      min_move_x: 20,min_move_y: 20,preventDefaultEvents: true
+    });
+    $('.thisphoto').touchwipe({
+      wipeRight: function() {iterate($(this).parent().attr('id'), -1);},
+      min_move_x: 20,min_move_y: 20,preventDefaultEvents: true
+    });
+
+
+});
--- src/media.md
+++ src/media.md
@@ -3,15 +3,31 @@ layout: page.html
 outdirs: '..'
 title: Media
 ---
+<div class='photo' id='bubbes'><span class='caption'>At The 6th Annual Bubbe's Old-Fashioned Hanukkah Shindig, Dec '17 <span class='counter'></span>.</span>
+<div class='thisphoto'>
+<span class='larr'><i class="fa fa-angle-left fa-2x"></i></span><span class='rarr'><i class="fa fa-angle-right fa-2x"></i></span>
+<figure><img src='../images/shows/bubbes/20171216_213355.jpg'></figure>
+<div class="leftside"></div>
+<div class="rightside"></div>
+</div>
+</div>
+
 <div id='soundcloudembed'>
 
-<div class='playlist'><span>A taste of our practice sessions.</span>
+<div class='playlist'><span class='caption'>Live at Muhabbet.</span>
+<iframe width="100%" height="300" scrolling="yes" frameborder="yes" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/360580616&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true&amp;show_teaser=true&amp;visual=true"></iframe></div>
+
+<div class='playlist'><span class='caption'>A taste of our practice sessions.</span>
 <iframe width="100%" height="300" scrolling="yes" frameborder="yes" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/365564623&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true&amp;show_teaser=true&amp;visual=true"></iframe></div>
 
-<div class='playlist'><span>Live at Muhabbet.</span>
-<iframe width="100%" height="300" scrolling="yes" frameborder="yes" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/360580616&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true&amp;show_teaser=true&amp;visual=true"></iframe></div>
 </div>
 
-<div class='photo'><span>At The 6th Annual Bubbe's Old-Fashioned Hanukkah Shindig.</span>
-<img width='100%' src='../images/shows/17-12-16-bubbes.jpg'>
+<div class='photo' id='misc'><span class='caption'>More shows 2017-'18 <span class='counter'></span>.</span>
+<div class='thisphoto'>
+<span class='larr'><i class="fa fa-angle-left fa-2x"></i></span><span class='rarr'><i class="fa fa-angle-right fa-2x"></i></span>
+<figure><img src='../images/shows/misc/houseshow 2018-1-20.jpg'></figure>
+<div class="leftside"></div>
+<div class="rightside"></div>
 </div>
+</div>
+
